a2712189313f6116ccd46632f7f73c14c9367b09
Abfly <shintree86@gmail.com>
2024-12-10 12:01:01 +0900

    v6.2

---
+++ js/background.js
@@ -0,0 +1,9 @@
+const images = ["blue.jpg", "green.jpg", "violet.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
