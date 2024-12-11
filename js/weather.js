923d020b4cce1dfeb27354f104a1a03b5a5e7a17
Abfly <shintree86@gmail.com>
2024-12-11 15:36:25 +0900

    v8.2

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = "a3d64e2fac45900bd72c51718495dc09";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("You live in.", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:last-child");
+      const weather = document.querySelector("#weather span:first-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp} / `;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
