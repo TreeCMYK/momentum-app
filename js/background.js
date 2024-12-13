beab25cab626e919105924be7764276f9ddf9eca
Abfly <shintree86@gmail.com>
2024-12-13 12:13:55 +0900

    v10.2

--- js/background.js
+++ js/background.js
@@ -5,5 +5,9 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+document.body.style.backgroundSize = "cover";
+document.body.style.backgroundPosition = "center";
+document.body.style.backgroundRepeat = "no-repeat";
 
+// document.body.style.height = "100vh";
 // document.body.appendChild(bgImage);
--- js/paint.js
+++ js/paint.js
@@ -1,3 +1,5 @@
+const cleanBtn = document.querySelector("#clean");
+const randomBtn = document.querySelector("#random");
 const saveBtn = document.querySelector("#save");
 const textInput = document.querySelector("#text");
 const fileInput = document.querySelector("#file");
@@ -9,12 +11,15 @@ const color = document.querySelector("#color");
 const lineWidth = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-const CANVAS_WIDTH = 800;
-const CANVAS_HEIGHT = 800;
+const CANVAS_WIDTH = 500;
+const CANVAS_HEIGHT = 500;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 ctx.lineCap = "round";
+
+const BGI_KEY = "backgroundimg";
+let bgi = [];
 
 let isPainting = false;
 let isFilling = false;
@@ -77,14 +82,6 @@ function onEraseClick() {
   modeBtn.innerText = "Fill";
 }
 function onFileChange(event) {
-  // const file = event.target.files[0];
-  // const url = URL.createObjectURL(file);
-  // const image = new Image();
-  // image.src = url;
-  // image.onload = function () {
-  //   ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  //   fileInput.value = null;
-  // };
   const file = event.target.files[0];
   const url = URL.createObjectURL(file);
   const image = new Image();
@@ -92,18 +89,12 @@ function onFileChange(event) {
   image.onload = function () {
     // 이미지와 캔버스의 비율 계산
     const scale = Math.min(CANVAS_WIDTH / image.width, CANVAS_HEIGHT / image.height);
-
     // 새로운 너비와 높이 계산
     const scWidth = image.width * scale;
     const scHeight = image.height * scale;
-
     // 이미지를 캔버스 중앙에 배치하기 위한 좌표 계산
     const x = (CANVAS_WIDTH - scWidth) / 2;
     const y = (CANVAS_HEIGHT - scHeight) / 2;
-
-    // 캔버스 초기화 (선택사항)
-    // ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
     // 이미지를 캔버스에 그리기
     ctx.drawImage(image, x, y, scWidth, scHeight);
     fileInput.value = null;
@@ -113,21 +104,43 @@ function onDoubleClick(event) {
   const text = textInput.value;
   if (text !== "") {
     ctx.save();
-    ctx.lineWidth = 1;
-    ctx.font = "48px serif";
+    const fontsize = Math.floor(ctx.lineWidth * 10);
+    ctx.font = `${fontsize}px Gluten`;
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
     ctx.fillText(text, event.offsetX, event.offsetY);
     ctx.restore();
   }
 }
-function onSaveClick() {
+function onSavePattern() {
+  // 패턴 저장
   const url = canvas.toDataURL();
-  // const a = document.createElement("a");
-  // a.href = url;
-  // a.download = "myDrawing.png";
-  // a.click();
-  document.body.style.backgroundImage = `url(${url})`;
+  const newbg = { id: Date.now(), img: url };
+  // 배열 저장
+  bgi.push(newbg);
+  // 로컬스토리지 저장
+  localStorage.setItem(BGI_KEY, JSON.stringify(bgi));
 }
-
+function onRandomPattern() {
+  // 패턴 랜덤
+  const paintChange = bgi[Math.floor(Math.random() * bgi.length)];
+  document.body.style.backgroundImage = `url(${paintChange.img})`;
+  document.body.style.backgroundSize = "";
+  document.body.style.backgroundRepeat = "repeat";
+}
+function onClean() {
+  // localStorage 초기화
+  localStorage.removeItem(BGI_KEY);
+  // 배열 초기화
+  bgi = [];
+  // 캔버스 초기화
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // 기존 배경 가져오기
+  const images = ["blue.jpg", "green.jpg", "violet.jpg"];
+  const chosenImage = images[Math.floor(Math.random() * images.length)];
+  document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+}
 canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
@@ -146,4 +159,6 @@ destroyBtn.addEventListener("click", onDestroyClick);
 eraseBtn.addEventListener("click", onEraseClick);
 
 fileInput.addEventListener("change", onFileChange);
-saveBtn.addEventListener("click", onSaveClick);
+saveBtn.addEventListener("click", onSavePattern);
+randomBtn.addEventListener("click", onRandomPattern);
+cleanBtn.addEventListener("click", onClean);
